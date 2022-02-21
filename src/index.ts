e102428eaf4345f53a0b18da3d2d183b6ab99666
benjamin <benjamin.d@make-it.fr>
2022-02-21 14:19:57 +0100

    import custom resolvers file & context in apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import 'reflect-metadata';
 import * as tq from 'type-graphql';
 import { ApolloServer } from 'apollo-server';
-import { resolvers } from '@generated/type-graphql';
+import { resolvers } from '../prisma/generated/type-graphql';
 import { context } from './context';
+
+import users from './custom-resolvers/users';
 
 const app = async () => {
   const schema = await tq.buildSchema({
-    resolvers,
+    resolvers: [...resolvers, users],
+    emitSchemaFile: true,
   });
 
-  new ApolloServer({ schema, context }).listen({ port: 4000 }, () =>
+  new ApolloServer({ schema, context: (req, res) => ({ prisma: context.prisma, req, res }) }).listen({ port: 4000 }, () =>
     console.log(
       `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-typegraphql-crud#using-the-graphql-api`
     )
