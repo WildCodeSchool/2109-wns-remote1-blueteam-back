4a365215deb81e0376779b0778d2cfabb1d95491
benjamin <benjamin.d@make-it.fr>
2022-02-22 17:45:47 +0100

    add express req & res in apollo context

--- src/context.ts
+++ src/context.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from '@prisma/client'
+import { Request, Response } from "express";
 
 const prisma = new PrismaClient()
 
 export interface Context {
   prisma: PrismaClient
+  req: Request<any, Record<string, any>>
+  res: Response<any, Record<string, any>>
 }
 
 export const context: Context = {
   prisma,
+  req: {} as Request,
+  res: {} as Response,
 }
