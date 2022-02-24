56779205f6105fd10b26fc096b430a66b8b21454
benjamin <benjamin.d@make-it.fr>
2022-02-24 11:45:02 +0100

    add rules ignore to eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,6 @@ module.exports = {
     tsconfigRootDir: './',
   },
   plugins: ['@typescript-eslint', 'prettier'],
-  rules: { 'no-underscore-dangle': ['error', { allow: ['_id'] }] },
+  rules: { 'no-underscore-dangle': ['error', { allow: ['_id'] }] , 'class-methods-use-this': 'off' },
   ignorePatterns: ['.eslintrc.js'],
 };
