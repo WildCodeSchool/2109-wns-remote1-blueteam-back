0989440c3dcff1204d270171afe7452f29f7be47
benjamin <benjamin.d@make-it.fr>
2022-03-30 17:25:07 +0200

    protect new collections with auth

--- src/add-crud-middleware.ts
+++ src/add-crud-middleware.ts
@@ -1,10 +1,29 @@
 import { UseMiddleware } from 'type-graphql';
-import { ResolversEnhanceMap, User } from '../prisma/generated/type-graphql';
+import {
+  ResolversEnhanceMap,
+  User,
+  Team,
+  Project,
+  ProjectComment,
+  File,
+} from '../prisma/generated/type-graphql';
 import checkToken from './middlewares/checkToken';
 import refreshToken from './middlewares/refreshToken';
 
 const mapped: ResolversEnhanceMap = {
   User: {
+    _all: [UseMiddleware(checkToken), UseMiddleware(refreshToken)],
+  },
+  Team: {
+    _all: [UseMiddleware(checkToken), UseMiddleware(refreshToken)],
+  },
+  Project: {
+    _all: [UseMiddleware(checkToken), UseMiddleware(refreshToken)],
+  },
+  ProjectComment: {
+    _all: [UseMiddleware(checkToken), UseMiddleware(refreshToken)],
+  },
+  File: {
     _all: [UseMiddleware(checkToken), UseMiddleware(refreshToken)],
   },
 };
